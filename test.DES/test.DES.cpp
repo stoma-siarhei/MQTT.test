378447bd184bdbc2c75c6b46081788b415028be4
Siarhei Stoma <scander@yandex.ru>
2024-08-06 14:41:34 +0800

    Добавлены тесты

--- test.DES/test.DES.cpp
+++ test.DES/test.DES.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <string>
 
 #include "DES.CBC.h"
 
@@ -8,5 +9,23 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    /*
+    * https://the-x.cn/en-us/cryptography/Des.aspx
+    */
+    {
+        crypto::DES des(0x8000000000000000);
+        if (auto _r = des.encrypt(0); _r == 0x95a8d72813daa94d) cout << "Encrypt - True" << endl;
+        else cout << "Encrypt - False" << endl;
+    }
+    {
+        if (auto _r = crypto::DES::decrypt(0x95a8d72813daa94d, 0x8000000000000000); _r == 0) cout << "Decrypt - True" << endl;
+        else cout << "Decrypt - False" << endl;
+    }
+    {
+        string str{ "texttext" };
+        uint64_t block = *((uint64_t*)str.data());
+        if (auto _r = crypto::DES::encrypt(block, block); _r == 0x6d3df7e513a79f66) cout << "Encrypt - True" << endl;
+        else cout << "Encrypt - False" << endl;
+    }
     return 0;
 }
