f2b707ebc63153c68cda033f726aaa9070c113d9
scander <scander@yandex.ru>
2024-08-06 22:54:16 +0800

    Написана функция проверки для третьего теста

--- test.crypto/test.crypto.cpp
+++ test.crypto/test.crypto.cpp
@@ -16,34 +16,12 @@ int main(int argc, char** argv)
     */
     {
         /*mTkvFxOd/NH9MEkOxY7czmeTu3OqTRgPRkdIwmUFHhfiO6eLEMud8k3Uc0XE4w==*/
-        string s{ "mTkvFxOd/NH9MEkOxY7czmeTu3OqTRgPRkdIwmUFHhfiO6eLEMud8k3Uc0XE4w==" };
-        crypto::Base64 base(s, crypto::Base64::Decode);
-        uint8_t* ptr = base.decode();
-        size_t size = base.size();
-        vector<uint64_t> v(size / 8 + 1);
-        vector<uint64_t> res;
-        memcpy((void*)v.data(), (void*)ptr, size);
-        string key{ "12345678" };
-        uint64_t* pkey = (uint64_t*)key.data();
-        crypto::DESCBC des(*pkey, *pkey);
-        for (auto&& it : v)
-        {
-            res.push_back(des.decrypt(it));
-        }
-        string str((char*)res.data());
-        cout << str << endl;
-    }
-    {
-        /*b5tdXnn7oyuXB22UG8WKFB0/5pXmyoZzKC8HgL5xL55y90vhl2gxLJ9KzCvLVL3yA14ABkr1gwYu2LFhb7J3AAfX+G/PP9kQG6BsJtWE412Riwu7H+k/BQ==*/
-    }
-    {
-        /*BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq*/
-        string data{ "BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq" };
+        string data{ "mTkvFxOd/NH9MEkOxY7czmeTu3OqTRgPRkdIwmUFHhfiO6eLEMud8k3Uc0XE4w==" };
         crypto::Base64 b{ data, crypto::Base64::Decode };
         uint8_t* p_data = b.decode();
         [[like]] if (auto size = b.size() - 1; size % 8 == 0)
         {
-            vector<uint64_t> v_data(size / 8), v_res(size / 8 + 1);
+            vector<uint64_t> v_data(size / 8), v_res(size / 8);
             v_res[v_res.size() - 1] = 0;
             memcpy((void*)v_data.data(), (void*)p_data, size);
             string key{ "87654321" };
@@ -51,6 +29,7 @@ int main(int argc, char** argv)
             crypto::DESCBC des(*p_key, *p_key);
             for (size_t i = 0; auto && it : v_data)
             {
+                if (i == v_res.size()) break;
                 uint64_t b = it;
                 uint8_t* _l = (uint8_t*)&b, * _r = (uint8_t*)&b;
                 for (int i = 0; i < 4; i++)
@@ -70,7 +49,80 @@ int main(int argc, char** argv)
         else
         {
             cout << "Error decode base64" << endl;
-            return 1;
+        }
+    }
+    {
+        /*b5tdXnn7oyuXB22UG8WKFB0/5pXmyoZzKC8HgL5xL55y90vhl2gxLJ9KzCvLVL3yA14ABkr1gwYu2LFhb7J3AAfX+G/PP9kQG6BsJtWE412Riwu7H+k/BQ==*/
+        string data{ "b5tdXnn7oyuXB22UG8WKFB0/5pXmyoZzKC8HgL5xL55y90vhl2gxLJ9KzCvLVL3yA14ABkr1gwYu2LFhb7J3AAfX+G/PP9kQG6BsJtWE412Riwu7H+k/BQ==" };
+        crypto::Base64 b{ data, crypto::Base64::Decode };
+        uint8_t* p_data = b.decode();
+        [[like]] if (auto size = b.size() - 1; size % 8 == 0)
+        {
+            vector<uint64_t> v_data(size / 8), v_res(size / 8);
+            v_res[v_res.size() - 1] = 0;
+            memcpy((void*)v_data.data(), (void*)p_data, size);
+            string key{ "87654321" };
+            uint64_t* p_key = (uint64_t*)key.data();
+            crypto::DESCBC des(*p_key, *p_key);
+            for (size_t i = 0; auto && it : v_data)
+            {
+                if (i == v_res.size()) break;
+                uint64_t b = it;
+                uint8_t* _l = (uint8_t*)&b, * _r = (uint8_t*)&b;
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                b = des.decrypt(b);
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                v_res[i++] = b;
+            }
+            string result{ (char*)&v_res[0] };
+            cout << result << endl;
+        }
+        else
+        {
+            cout << "Error decode base64" << endl;
+        }
+    }
+    {
+        /*BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq*/
+        string data{ "BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq" };
+        crypto::Base64 b{ data, crypto::Base64::Decode };
+        uint8_t* p_data = b.decode();
+        [[like]] if (auto size = b.size() - 1; size % 8 == 0)
+        {
+            vector<uint64_t> v_data(size / 8), v_res(size / 8);
+            v_res[v_res.size() - 1] = 0;
+            memcpy((void*)v_data.data(), (void*)p_data, size);
+            string key{ "87654321" };
+            uint64_t* p_key = (uint64_t*)key.data();
+            crypto::DESCBC des(*p_key, *p_key);
+            for (size_t i = 0; auto && it : v_data)
+            {
+                if (i == v_res.size()) break;
+                uint64_t b = it;
+                uint8_t* _l = (uint8_t*)&b, * _r = (uint8_t*)&b;
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                b = des.decrypt(b);
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                v_res[i++] = b;
+            }
+            string result{ (char*)&v_res[0] };
+            cout << result << endl;
+        }
+        else
+        {
+            cout << "Error decode base64" << endl;
         }
     }
     return 0;
