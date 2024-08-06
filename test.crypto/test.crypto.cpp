6dc4eded0a0e9b8c672667b22fcaffc220f447c8
scander <scander@yandex.ru>
2024-08-06 22:36:13 +0800

    Написана функция проверки для третьего теста

--- test.crypto/test.crypto.cpp
+++ test.crypto/test.crypto.cpp
@@ -11,6 +11,9 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    /*
+    * https://the-x.cn/en-us/cryptography/Des.aspx
+    */
     {
         /*mTkvFxOd/NH9MEkOxY7czmeTu3OqTRgPRkdIwmUFHhfiO6eLEMud8k3Uc0XE4w==*/
         string s{ "mTkvFxOd/NH9MEkOxY7czmeTu3OqTRgPRkdIwmUFHhfiO6eLEMud8k3Uc0XE4w==" };
@@ -35,6 +38,40 @@ int main(int argc, char** argv)
     }
     {
         /*BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq*/
+        string data{ "BmcA0GkPBMv2Kpzu0V6W2kfDiNIGuM9iHDPxrCIOKfsdGoItzduC5oYBjrBAXMuW4enfWr2GJ/jd4PRET5/w9ovhX1U4o2MDSl9zzOpQi31yfzCKl5iDqcZ3H7lbOELq" };
+        crypto::Base64 b{ data, crypto::Base64::Decode };
+        uint8_t* p_data = b.decode();
+        [[like]] if (auto size = b.size() - 1; size % 8 == 0)
+        {
+            vector<uint64_t> v_data(size / 8), v_res(size / 8 + 1);
+            v_res[v_res.size() - 1] = 0;
+            memcpy((void*)v_data.data(), (void*)p_data, size);
+            string key{ "87654321" };
+            uint64_t* p_key = (uint64_t*)key.data();
+            crypto::DESCBC des(*p_key, *p_key);
+            for (size_t i = 0; auto && it : v_data)
+            {
+                uint64_t b = it;
+                uint8_t* _l = (uint8_t*)&b, * _r = (uint8_t*)&b;
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                b = des.decrypt(b);
+                for (int i = 0; i < 4; i++)
+                {
+                    std::swap(*(_l + i), *(_r + 7 - i));
+                }
+                v_res[i++] = b;
+            }
+            string result{ (char*)&v_res[0] };
+            cout << result << endl;
+        }
+        else
+        {
+            cout << "Error decode base64" << endl;
+            return 1;
+        }
     }
     return 0;
 }
